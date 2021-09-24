7a0fc9c6093c3601bfd1e9d85ede193f06c062a1
Michael Mason <michael.mason7194@gmail.com>
2021-09-24 18:02:36 -0400

    Updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private router: Router) {}
+export class AppComponent implements OnInit {
+  screenSize$: Observable<string>;
+
+  constructor(private router: Router, private mediaObserver: MediaObserver) {}
+
+  ngOnInit(): void {
+    this.screenSize$ = this.mediaObserver.asObservable().pipe(
+      map((obs) => obs[0].mqAlias),
+      tap(console.log)
+    );
+  }
 
   navToMichaelWMason95(): void {
     location.href = 'http://michaelwmason95.com';
